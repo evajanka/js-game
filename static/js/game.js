b254704830f2dcc03e591d9522ad625227892cb6
evajanka <robert.eva93@gmail.com>
2019-09-24 10:37:53 +0200

    random fruit

--- static/js/game.js
+++ static/js/game.js
@@ -10,6 +10,28 @@ function move() {
     x += 1;
     pos = "x" + x + "y" + y;
     document.getElementById(pos).style.backgroundColor = "red";
+
 }
 
-game();
+
+
+function randomize_fruit() {
+    let score = document.createElement("img");
+    score.src = 'static/apple.jpeg'
+    score.setAttribute("width", "20");
+    score.setAttribute("height", "20");
+    let fruit_to_put =  document.getElementById("table").rows[i].cells;
+    fruit_to_put[i].appendChild(score);
+
+}
+
+
+window.addEventListener('load', () => { let i = Math.floor(Math.random() * 31);
+    let score = document.createElement("img");
+    score.src = 'static/apple.jpeg'
+    score.setAttribute("width", "20");
+    score.setAttribute("height", "20");
+    let fruit_to_put =  document.getElementById("table").rows[i].cells;
+    fruit_to_put[i].appendChild(score);});
+
+//game();
